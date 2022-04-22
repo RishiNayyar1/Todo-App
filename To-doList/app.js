3685b97b9bc4a1abf6d18c9a472afcfd37da0cc5
RishiNayyar <rishinayyar68@gmail.com>
2022-04-22 14:11:14 +0530

    add css to lists and takes input from user

--- To-doList/app.js
+++ To-doList/app.js
@@ -20,22 +20,27 @@ function addTodo(event){
     
     //create list
     const newTodo = document.createElement("li");
-    newTodo.innerText = 'hey';
+    newTodo.innerText = todoInput.value; //takes input from the user 
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
 
     //create mark button
     const completedButton = document.createElement("button");
-    completedButton.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    completedButton.innerHTML = '<i class="fa-solid fa-check"></i>';
     completedButton.classList.add("complete-btn");
     todoDiv.appendChild(completedButton);
     
     //create trash button
     const trashButton = document.createElement("button");
     trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
-    trashButton.classList.add("complete-btn");
+    trashButton.classList.add("trash-btn");
     todoDiv.appendChild(trashButton);
 
     //append to list
     todoList.append(todoDiv);
+
+    //clear todo input value
+ todoInput.value = ""; 
+
+
 }
